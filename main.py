39bcf82adf3a67c389e1313b3d52dd79d58e73eb
VincentRPS <vincentbusiness55@gmail.com>
2022-06-12 14:52:36 +0800

    fix: middleware

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from fastapi import FastAPI
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from slowapi.util import get_remote_address
+from slowapi.middleware import SlowAPIMiddleware
 from dotenv import load_dotenv
 
 from manhattan.database import connect
@@ -40,6 +41,7 @@ limiter = Limiter(
 app = FastAPI()
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
 app.include_router(users_router)
 
 
